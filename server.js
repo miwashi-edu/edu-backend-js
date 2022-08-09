d71e7d5ce6bb8cdc1d79e4021f3db77e7b9d9d72
Mikael Wallin <mikael.x.wallin@gmail.com>
2022-08-09 22:53:57 +0200

    Working JWT

--- server.js
+++ server.js
@@ -1,15 +1,53 @@
+require('dotenv').config()
 const express = require('express')
-const app = express()
+const jwt = require('jsonwebtoken')
+const healthcheck = require('healthcheck')
+
+//const bodyparser = reuire('bodyparser')
+
+//console.log(process.env)
 const PORT = process.env.PORT || 3000
+ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET
 
-//app.use('/healthcheck', require('./routes/healthcheck.routes'));
+console.log('****** NODE_ENV *******')
+console.log(process.env.NODE_ENV)
+console.log('****** NODE_ENV *******')
 
-app.get("/", (req ,res)=>{
-   headers={"http_status":200, "cache-control":  "no-cache"}
+const app = express()
+app.use(express.json())
+app.use('/healthcheck', require('./routes/healthcheck.routes'));
+
+app.get("/", (req ,res) => {
+   headers={"cache-control":  "no-cache"}
    body={"status": "available"}
-   res.status(200).send(body)
+   res.status(200).json(body)
 })
+
+app.post("/authenticate", (req ,res) => {
+    headers={"cache-control":  "no-cache"}
+    const username = req.body.username
+    const accessToken = jwt.sign({ name: username }, ACCESS_TOKEN_SECRET)
+    console.log(jwt.json)
+    res.status(200).json({accessToken: accessToken})
+ })
+
+ app.get("/secret", authenticateToken, (req ,res) => {
+    console.log("In secret")
+    res.json({user: req.user.name})
+ })
 
 app.listen(PORT , ()=>{
      console.log(`STARTED LISTENING ON PORT ${PORT}`)
 });
+
+function authenticateToken(req, res, next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if(token == null) return  res.sendStatus(401)
+
+    jwt.verify(token, ACCESS_TOKEN_SECRET, (err, user) => {
+        if(err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+}
